f933a1f889a6a3e51c806c1cc437e8d5bfb553a9
Averagebursmeseguy <115561450+Averagebursmeseguy@users.noreply.github.com>
2026-03-18 20:56:00 +0800

    Started making a placeholder to demonstrate functionality and basic structure.

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import tkinter
 import pygame
+import random
 
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
 running = True
+rocket = pygame.image.load("./assets/placeholder_rocket.png")
+
+y = 600
+random_coords = random.choice([200, 500, 300, 400, 700, 800])
 
 while running:
 
-    screen.fill((0, 0, 0))
+    screen.fill((20, 20, 20))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    if y > 0:
+        y -= 1
+    else:
+        y = 600
+        random_coords = random.choice([200, 500, 300, 400, 700, 800])
 
+    print(y)
+    screen.blit(rocket, (random_coords, y))
 
+    pygame.display.flip() #renders all the sprites and displays them in window. Don't fuck with this one.
 pygame.quit()
