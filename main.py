68fb4b8b20c9574ef5ca15417f83283b82fac1bb
Averagebursmeseguy <115561450+Averagebursmeseguy@users.noreply.github.com>
2026-03-18 19:52:30 +0800

    First ever commit! Yaayyyy

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+
+while running:
+
+    screen.fill((0, 0, 0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+pygame.quit()
