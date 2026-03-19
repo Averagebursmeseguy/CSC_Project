ba3e26040af2e32c004e615d9158517610ad8f24
Averagebursmeseguy <115561450+Averagebursmeseguy@users.noreply.github.com>
2026-03-19 19:43:38 +0800

    started testing out map generation for game

--- main.py
+++ main.py
@@ -4,30 +4,47 @@ import random
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1000, 1000))
 
 running = True
-rocket = pygame.image.load("./assets/placeholder_rocket.png")
+rocket = pygame.image.load("./assets/placeholder_rocket.png").convert_alpha()
 
-y = 600
-random_coords = random.choice([200, 500, 300, 400, 700, 800])
+def drawGrid(startpos, endpos, scaling, surface, linecolor):
+
+    startx, starty = startpos
+    endx, endy = endpos
+
+    #Make vertical and horiz lines first
+    pygame.draw.line(surface, linecolor, (startx, starty), (endx, starty), 1)
+    pygame.draw.line(surface, linecolor, (startx, starty), (startx, endy), 1)
+    pygame.draw.line(surface, linecolor, (endx, endy), (startx, endy), 1)
+    pygame.draw.line(surface, linecolor, (endx, endy), (endx, starty), 1)
+
+    for i in range(int(endx / scaling)):
+        increment = startx + (scaling * i)
+        pygame.draw.line(surface, linecolor, (increment, starty), (increment, endy))
+
+    for i in range(int(endy / scaling)):
+        increment = starty + (scaling * i)
+        pygame.draw.line(surface, linecolor, (startx, increment), (endx, increment))
+
 
 while running:
 
-    screen.fill((20, 20, 20))
-    
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    if y > 0:
-        y -= 1
-    else:
-        y = 600
-        random_coords = random.choice([200, 500, 300, 400, 700, 800])
+    drawGrid((100, 100), (700, 700), 50, screen, (255, 255, 255))
+    
+    # pygame.draw.line(screen, (255, 255, 255), (100, 100), (700, 100), 1)
+    # pygame.draw.line(screen, (255, 255, 255), (100, 100), (100, 700), 1)
 
-    print(y)
-    screen.blit(rocket, (random_coords, y))
+
+    
+    
+
 
     pygame.display.flip() #renders all the sprites and displays them in window. Don't fuck with this one.
 pygame.quit()
