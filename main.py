9c8f37e944c240548f1d865f50bb2659f87250bf
Averagebursmeseguy <115561450+Averagebursmeseguy@users.noreply.github.com>
2026-03-25 04:14:47 +0800

    offloaded map-related scripts and fixed a funny mistake with some bugs

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
 import tkinter
 import pygame
 import random
+import map_utils
 
 pygame.init()
 
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((800, 600))
 
 running = True
 rocket = pygame.image.load("./assets/placeholder_rocket.png").convert_alpha()
 
-def drawGrid(startpos, endpos, scaling, surface, linecolor):
 
-    startx, starty = startpos
-    endx, endy = endpos
-
-    #Make vertical and horiz lines first
-    pygame.draw.line(surface, linecolor, (startx, starty), (endx, starty), 1)
-    pygame.draw.line(surface, linecolor, (startx, starty), (startx, endy), 1)
-    pygame.draw.line(surface, linecolor, (endx, endy), (startx, endy), 1)
-    pygame.draw.line(surface, linecolor, (endx, endy), (endx, starty), 1)
-
-    for i in range(int(endx / scaling)):
-        increment = startx + (scaling * i)
-        pygame.draw.line(surface, linecolor, (increment, starty), (increment, endy))
-
-    for i in range(int(endy / scaling)):
-        increment = starty + (scaling * i)
-        pygame.draw.line(surface, linecolor, (startx, increment), (endx, increment))
-
+print(map_utils.centreFinder(screen))
 
 while running:
 
@@ -36,15 +20,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    drawGrid((100, 100), (700, 700), 50, screen, (255, 255, 255))
+    map_utils.drawGrid((100, 100), (700, 700), 50, screen, (255, 255, 255))
     
     # pygame.draw.line(screen, (255, 255, 255), (100, 100), (700, 100), 1)
     # pygame.draw.line(screen, (255, 255, 255), (100, 100), (100, 700), 1)
-
-
-    
-    
-
 
     pygame.display.flip() #renders all the sprites and displays them in window. Don't fuck with this one.
 pygame.quit()
